62ddbb56aa985e1e9a6c258979766a9d289400af
GokulPSVG <creatcoc0001@gmail.com>
2023-10-26 15:36:33 +0530

    15th commit

--- aihub1/src/AiPopUpPage.js
+++ aihub1/src/AiPopUpPage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { AiOutlineCloseCircle } from "react-icons/ai";
 
 const AiPopUpPage = ({singleAI,settoggle}) => {
-    const path=singleAI.path
+    
   return (
     <div className='AiPopUp'>
        <span className='PopUpTop'>
@@ -12,9 +12,6 @@ const AiPopUpPage = ({singleAI,settoggle}) => {
             />
         </span>
         <p>{singleAI.description}</p>
-        <div className='popupLogo'>
-            <img alt='logo' src={require(`${path}`)}></img>
-        </div>
         <div className='visitLink'>
             <a href={singleAI.link} >Visit</a>
         </div>
