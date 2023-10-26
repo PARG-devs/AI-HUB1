0bdaae909d14a43993a5db2bdb28e89347877382
GokulPSVG <creatcoc0001@gmail.com>
2023-10-26 15:34:02 +0530

    14th commit

--- aihub1/src/ListItems.js
+++ aihub1/src/ListItems.js
@@ -1,10 +1,14 @@
 import React, {useState } from 'react'
+import AiPopUpPage from './AiPopUpPage'
 const ListItems = ({singleAI}) => {
   const [toggle,settoggle]=useState(false)
   const path=singleAI.path
   return (
     <div className='singleAIButton'>
-      {toggle? ''
+      {toggle? <AiPopUpPage 
+                  singleAI={singleAI} 
+                  settoggle={settoggle}      
+               /> 
       : ''}
         <li className='aiButton'>
           <button
